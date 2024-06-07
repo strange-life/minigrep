bc5bcb931136bdff08cb3be0b0fcde389a1c91cc
HarperHuang <strange.hao@gmail.com>
2024-06-07 14:01:30 +0800

    feat: search in file

---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+use std::{env, error::Error, fs};
+
+pub struct Config {
+    pub file_path: String,
+    pub query: String,
+}
+
+pub fn get_config() -> Result<Config, &'static str> {
+    let mut args = env::args().skip(1);
+
+    let Some(file_path) = args.next() else {
+        return Err("no file path");
+    };
+
+    let Some(query) = args.next() else {
+        return Err("no query");
+    };
+
+    Ok(Config { file_path, query })
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&contents, &config.query) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+fn search<'a>(contents: &'a str, query: &str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let contents = "Rust:
+safe, fast, productive.
+Pick three.";
+
+        let query = "duct";
+
+        assert_eq!(vec!["safe, fast, productive."], search(contents, query));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use minigrep::get_config;
+use minigrep::run;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let config = get_config().unwrap_or_else(|e| {
+        eprintln!("Parse arguments failed: {e}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Search failed: {e}");
+        process::exit(1);
+    }
 }

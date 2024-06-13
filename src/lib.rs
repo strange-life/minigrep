2e7cde3c539551ee640799e7cc17a12267a5cfb4
HarperHuang <strange.hao@gmail.com>
2024-06-13 16:10:48 +0800

    feat: add ignore_case

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,14 @@ use std::{error::Error, fs};
 pub struct Config {
     pub path: String,
     pub query: String,
+    pub ignore_case: bool,
 }
 
 pub fn parse_config(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
     let mut config = Config {
         path: String::new(),
         query: String::new(),
+        ignore_case: false,
     };
 
     while let Some(arg) = args.next() {
@@ -22,6 +24,9 @@ pub fn parse_config(mut args: impl Iterator<Item = String>) -> Result<Config, &'
                 if let Some(query) = args.next() {
                     config.query = query;
                 }
+            }
+            "-i" | "--ignore_case" => {
+                config.ignore_case = true;
             }
             _ => (),
         }
@@ -41,15 +46,25 @@ pub fn parse_config(mut args: impl Iterator<Item = String>) -> Result<Config, &'
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(&config.path)?;
 
-    for line in search(&content, &config.query) {
+    for line in search(&content, &config.query, config.ignore_case) {
         println!("{line}");
     }
 
     Ok(())
 }
 
-fn search<'a>(content: &'a str, query: &'a str) -> impl Iterator<Item = &'a str> {
-    content.lines().filter(move |line| line.contains(query))
+fn search<'a>(
+    content: &'a str,
+    query: &'a str,
+    ignore_case: bool,
+) -> impl Iterator<Item = &'a str> {
+    content.lines().filter(move |line| {
+        if ignore_case {
+            line.to_lowercase().contains(&query.to_lowercase())
+        } else {
+            line.contains(query)
+        }
+    })
 }
 
 #[cfg(test)]
@@ -74,12 +89,39 @@ mod tests {
 
     #[test]
     fn one_result() {
-        let content = "Rust:
+        let content = "\
+Rust:
 safe, fast, productive.
 Pick three.";
         let query = "duct";
-        let results: Vec<_> = search(content, query).collect();
+        let results: Vec<_> = search(content, query, false).collect();
 
         assert_eq!(results, vec!["safe, fast, productive."]);
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+        let query = "duct";
+        let results: Vec<_> = search(content, query, false).collect();
+
+        assert_eq!(results, vec!["safe, fast, productive."]);
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        let query = "rUsT";
+        let results: Vec<_> = search(content, query, true).collect();
+
+        assert_eq!(results, vec!["Rust:", "Trust me."],);
     }
 }
